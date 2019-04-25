89ec7602a4650b3680cb9e2e740f494a60acc5db
Julkarnine Tamim <38746644+julkarninetamim@users.noreply.github.com>
2019-04-25 23:56:33 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+//To Print Code in PHP(with echo)
+
+echo "Introduction to PHP";
+
+ //Data type: String,Integer
+
+$name= "Julkarnine Tamim";
+
+echo $name;
+
+//Data Type: Integer;
+
+$number1=10;
+$number2=20;
+
+echo $number1+$number2;
+
+
+// Variable >> variable data assign
+
+define('Age',20);
+
+echo Age;
+
+define('country','Bangladesh');
+echo country;
+
+//Constant >> constant data assign
+
+define('Age1',20);
+echo Age1;
+
+//Difference betweenn signle('') and double("") cotesion
+
+
+$value1= "hello ";
+$value2= "Bangladesh";
+ echo $value1.$value2;
