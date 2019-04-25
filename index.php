37bf4bd6be6a1c944f0c8971b1a12eca08328d72
Julkarnine Tamim <38746644+julkarninetamim@users.noreply.github.com>
2019-04-26 00:21:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,60 @@ echo Age1;
 //Difference betweenn signle('') and double("") cotesion
 
 
-$value1= "hello ";
-$value2= "Bangladesh";
- echo $value1.$value2;
+
+
+/*With single quote there is no expanding of the variables and we 
+cannot use the escape sequence for special characters 
+ like (\n,\t,etc)neither.check:*/
+
+    echo 'hi \n how are you.' ;
+    //Output: hi \n how are you
+    $var = 'Bob' ;
+    echo '$var' ;
+    //Output: $var
+
+
+//However, we can escape the ' and / sign by using /'
+
+    echo 'How/'re doing?'
+    //Output: How're doing?
+     
+     /*With double quote the variables can be expanded and we can use 
+     the escape sequence for special characters like \n,\r,\t,etc.*/
+
+    echo "hi \n how are you.";
+    //Output: hi
+    //            how are you.
+    $var = 'Bob';
+    echo "$var";
+    //Output: Bob
+
+//Function in PHP
+
+function addNumbers(int $a, int $b) {
+    return $a + $b;//its returns the value out of the function
+}
+echo addNumbers(5, "5 days"); 
+// since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
+
+
+//PHP Default Argument Value
+declare(strict_types=1);
+function setHeight(int $minheight = 50) {
+    echo "The height is : $minheight <br>";
+}
+
+setHeight(350);
+setHeight();
+setHeight(135);
+setHeight(80);
+/*
+OUTOUT IS:
+The height is : 350
+The height is : 50
+The height is : 135
+The height is : 80 
+
+*/
+
+
